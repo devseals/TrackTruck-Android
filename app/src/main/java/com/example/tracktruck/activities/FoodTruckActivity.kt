12f674f05bfa4d1bd2545a72fe6d3165a30816f9
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2019-05-25 17:30:23 -0500

    Funcionalidad de contacto

--- app/src/main/java/com/example/tracktruck/activities/FoodTruckActivity.kt
+++ app/src/main/java/com/example/tracktruck/activities/FoodTruckActivity.kt
@@ -1,29 +1,18 @@
 package com.example.tracktruck.activities
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import androidx.viewpager.widget.ViewPager
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.tracktruck.R
-import com.example.tracktruck.activities.ui.main.SectionsPagerAdapter
-import com.example.tracktruck.activities.ui.main.SectionsPagerAdapter2
-import com.example.tracktruck.fragments.ReviewsFragment
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.food_truck_view.*
-
-
 
 class FoodTruckActivity : AppCompatActivity(){
 
@@ -31,6 +20,9 @@ class FoodTruckActivity : AppCompatActivity(){
     private var longitude : Double = 0.0
     private var foodtruckName : String = ""
     private var foodtruckId : Int = 0
+    private var phoneNumber : String = ""
+    private val MY_PERMISSIONS_REQUEST_CALL_PHONE = 1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,12 +35,18 @@ class FoodTruckActivity : AppCompatActivity(){
             foodTypeViewText.text=getString("food_type")
             avgCostViewText.text="S/ " + getDouble("avg_price").toString()
             foodtruckId= getInt("foodtruck_id")
+            phoneNumber = getString("phone_number")
             phoneNumbertext.text=getString("phone_number")
         }
 
         setSupportActionBar(toolbar2)
         val actionBar = supportActionBar
         actionBar!!.title = foodtruckName
+
+        val phoneNumbertext = findViewById(R.id.phoneNumbertext) as TextView
+        phoneNumbertext.setOnClickListener{
+            call()
+        }
     }
 
     fun location()
@@ -66,7 +64,6 @@ class FoodTruckActivity : AppCompatActivity(){
         return super.onCreateOptionsMenu(menu)
     }
 
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle presses on the action bar menu items
         when (item.itemId) {
@@ -78,224 +75,41 @@ class FoodTruckActivity : AppCompatActivity(){
         return super.onOptionsItemSelected(item)
     }
 
-    public fun getFoodTruckId(): Int {
-        return foodtruckId
+    fun call(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE),MY_PERMISSIONS_REQUEST_CALL_PHONE)
+        }
+        else
+        {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:" + phoneNumber)
+            startActivity(intent)
+        }
+
+
     }
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_CALL_PHONE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
